fc13e3fcae607508e46ee6b086be50c7429823fe
Dzhoy Zuckerman <dzhoyz@gmail.com>
2019-12-08 22:35:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -15,14 +15,14 @@ end
 def find_greater_pair(src)
 new_array = []
 row_index = 0
-while row_index < src.length do
-  if src[row_index][0] > src[row_index][1]
-    new_array << src[row_index][0]
-  else
-    new_array << src[row_index][1]
+  while row_index < src.length do
+    if src[row_index][0] > src[row_index][1]
+      new_array << src[row_index][0]
+    else
+      new_array << src[row_index][1]
+    end
+    row_index += 1
   end
-  row_index += 1
-end
   new_array
 end
 
